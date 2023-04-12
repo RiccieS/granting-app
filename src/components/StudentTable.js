fddcee5f688d409b165eb146f42d0fffafa856e6
RiccieS <riccigames82@gmail.com>
2023-04-12 21:11:20 +0200

    Update StudentTable.js

--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -9,16 +9,18 @@ export default function StudentTable({ data }) {
           <th>HODNOST</th>
           <th>PRIJMENI</th>
           <th>JMENO</th>
-          <th>ZNAMKA</th>
           <th>STUDIJNI SKUPINA</th>
+          <th>ZNAMKA</th>
+          
         </tr>
       </thead>
       <tbody>
         {data.map(user => (
           <tr key={user.id}>
+            <td>{user.id}</td>
             <td>{user.hodnost}</td>
-            <td>{user.jmeno}</td>
             <td>{user.prijmeni}</td>
+            <td>{user.jmeno}</td>
             <td>{user.studjini_skupina}</td>
             <td>{user.znamka}</td>
 
