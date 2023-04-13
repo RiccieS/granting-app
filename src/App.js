e90a996d4fb461497dd9d76355ba180cfe09d7b9
RiccieS <riccigames82@gmail.com>
2023-04-13 20:50:25 +0200

    zkouska

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import StudentTable from './components/StudentTable';
 import StudentSelect from './components/StudentSelect';
 import data from './data/students.json';
-
+//ZKOUSKA BRANCH
 export default function App() {
   const [selectedStudent, setSelectedStudent] = useState(data[0]);//Tady to editneme aby to brala uzivatele dle účtu ze kterého se přihlásil data[0] specifikuje kdo bude zobrazen
 
