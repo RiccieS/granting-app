53e5a951c49f4fecdfe8e06726f24ae634425b9e
RiccieS <riccigames82@gmail.com>
2023-04-13 20:40:59 +0200

    dataset znamky

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import StudentTable from './components/StudentTable';
 import StudentSelect from './components/StudentSelect';
-import data from './data/dataset.json';
+import data from './data/students.json';
 
 export default function App() {
   const [selectedStudent, setSelectedStudent] = useState(data[0]);//Tady to editneme aby to brala uzivatele dle účtu ze kterého se přihlásil data[0] specifikuje kdo bude zobrazen
