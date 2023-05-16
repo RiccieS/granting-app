21202e088c8c999db06ee62e7bcb2839b5c22ac4
Vitek233 <vitekrout@gmail.com>
2023-05-16 19:25:22 +0200

    Úprava kódu
    Výběr oboru mění známky

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import StudentSelect from './components/StudentSelect';
 import data from './data/students.json';
 import grades_data from './data/grades.json';
 import BranchSelect from './components/BranchSelect';
-//import BranchTable from './components/BranchTable';
 import GradesTable from './components/GradesTable';
 import SemesterSelect from './components/SemesterSelect';
+import AddEntryForm from './components/AddEntryForm';
 
 export default function App() {
-  const [selectedStudent, setSelectedStudent] = useState(data[0]); //Tady to později upravíme aby to brala uzivatele dle účtu ze kterého se přihlásil, data[0] specifikuje kdo bude zobrazen
-  const [selectedBranch, setSelectedBranch] = useState(data[0]); //State pro zvolenou studijní skupinu
-  const [selectedSemester, setSelectedSemester] = useState('all');//State pro zvolený semestr
-  //Funkce filtrující data pro zvoleného studenta
+  const [selectedStudent, setSelectedStudent] = useState(data[0]); 
+  const [selectedBranch, setSelectedBranch] = useState(data[0]); 
+  const [selectedSemester, setSelectedSemester] = useState('all');
+
   const filterData = (student) => {
     if (student) {
       return data.filter((item) => item.id === student.id);
@@ -21,18 +21,17 @@ export default function App() {
     }
   };
 
-  const filteredData = filterData(selectedStudent);//Filtrování dat podle zvoleného studenta
+  const filteredData = filterData(selectedStudent);
 
-  /*let filteredGradesData = grades_data.filter((item) => item.id_student === selectedStudent.id); //Filtr pro zobrazeni grades prislusnemu studentovi
-
-  if (selectedSemester !== 'all') {
-    filteredGradesData = filteredGradesData.filter((item) => item.semester === selectedSemester);
-  }*/
-  //Filtr pro zobrazení známek příslušnému studentovi
   const filteredGradesData = grades_data.filter(
     (item) => item.id_student === selectedStudent.id && 
               (selectedSemester === 'all' || item.semester === parseInt(selectedSemester))
   );
+
+  const handleAddEntry = (entry) => {
+    // Add the new entry to the grades_data JSON file
+    // You can use the same method you used in the AddEntryForm component to add the new entry
+  }
 
   return (
     <div>
@@ -43,19 +42,20 @@ export default function App() {
         <div className="card">
           <h2 className="card-header">Přehled studentů</h2>
           <div className="card-body">
-            <BranchSelect data={data} grades_data={grades_data} selectedBranch={selectedBranch} onBranchChange={setSelectedBranch} /> {/* Komponenta volby studijních skupin */}
-            <StudentSelect selectedStudent={selectedStudent} onStudentChange={setSelectedStudent} data={data} /> {/* Komponenta výběru studenta*/}
+            <BranchSelect data={data} grades_data={grades_data} selectedBranch={selectedBranch} onBranchChange={setSelectedBranch} />
+            <StudentSelect selectedStudent={selectedStudent} onStudentChange={setSelectedStudent} data={data} />
             <div>
-              <StudentTable data={filteredData} /> {/* Tabulka vypisující vybrané studenty tzn. jednoho */}
+              <StudentTable data={filteredData} />
             </div>
           </div>
         </div>
         <div className="card">
           <h2 className="card-header">Přehled známek</h2>
           <div className="card-body">
-            <SemesterSelect selectedSemester={selectedSemester} onSemesterChange={setSelectedSemester} /> {/* Komponenta pro výběr semestru */}
+            <SemesterSelect selectedSemester={selectedSemester} onSemesterChange={setSelectedSemester} />
             <div>
-              <GradesTable data={filteredGradesData} /> {/* Tabulka s výpisem známek za semestr */}
+              <GradesTable data={filteredGradesData} />
+              <AddEntryForm onAddEntry={handleAddEntry} />
             </div>
           </div>
         </div>
--- src/components/BranchGradeTable.js
+++ src/components/BranchGradeTable.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+//UPRAVIT KOD filteredGrades je mozna zbytecne
 export default function BranchGradeTable({ data, grades_data, studjiniSkupina }) {
   const filteredStudents = data.filter((student) => student.studjini_skupina === studjiniSkupina); // Filtrace studentů na zákaldě stuijní skupiny
   const filteredGrades = grades_data.filter((grade) => filteredStudents.some((student) => student.id === grade.id_student)); //Filtrace známek studentů na základě jejich ID
+  
+  // Vytvoření objektu, klíč je název předmětu, hodnoty jsou pole známek předmětu
+  const gradesBySubject = filteredGrades.reduce((result, grade) => {
+    if (result[grade.subject]) {
+      result[grade.subject].push(grade.grade);
+    } else {
+      result[grade.subject] = [grade.grade];
+    }
+    return result;
+  }, {});
 
-  return (//Vykreslení tabulky s využitím stylů bootsrap
+  // Vytvoření pole objektů, kde každý objekt obsahuje název předmětu a průměrnou známku pro daný předmět
+  const averageGrades = Object.entries(gradesBySubject).map(([subject, grades]) => ({
+    subject,
+    average: grades.reduce((sum, grade) => sum + grade, 0) / grades.length,
+  }));
+
+  return (
     <table className="table table-striped table-hover table-sm">
       <caption>Studijní skupina: {studjiniSkupina}</caption>
       <thead>
@@ -15,10 +31,10 @@ export default function BranchGradeTable({ data, grades_data, studjiniSkupina })
         </tr>
       </thead>
       <tbody>
-        {filteredGrades.map((grade) => (//Projde pole filtorvaných známek a pro každý prvek vytvoří nový řádek
-          <tr >
+        {averageGrades.map((grade) => (
+          <tr key={grade.subject}>
             <td>{grade.subject}</td>
-            <td>{grade.grade}</td>
+            <td>{grade.average.toFixed(2)}</td>
           </tr>
         ))}
       </tbody>
