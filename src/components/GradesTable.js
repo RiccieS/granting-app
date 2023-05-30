8b1b56b5bea7a30df154414c47af8907b8409e38
RiccieS <riccigames82@gmail.com>
2023-05-30 23:14:54 +0200

    odlozeni

--- src/components/GradesTable.js
+++ src/components/GradesTable.js
@@ -1,35 +1,8 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function GradesTable({ data }) {
-  return (
-    <table className="table table-striped table-hover table-sm">
-      <thead>
-        <tr>
-          <th>SEMESTR</th> {/*záhlaví sloupců */}
-          <th>PREDMET</th> 
-          <th>ZNAMKA</th> 
-          <th>ID STUDENT</th> 
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(grade => ( // výpis dat ze vstupního pole data
-          <tr key={grade.id_grade}> {/* unikátní klíč pro každý řádek tabulky*/}
-            <td>{grade.semester}</td> {/*data pro sloupce */}
-            <td>{grade.subject}</td> 
-            <td>{grade.grade}</td> 
-            <td>{grade.id_student}</td> 
-          </tr>
-        ))}
-      </tbody>
-    </table>
-=======
 import React, { useEffect, useState } from 'react';
 import { GradesQuery } from '../queries/GradesQuery';
+import { GradesLevelsQuery } from '../queries/GradesLevelsQuery';
 import { authorizedFetch } from '../queries/authorizedFetch';
 import { ClassificationUpdateMutation } from '../queries/ClassificationUpdateMutation';
-import optionLevels from '../queries/fakeQueryLevels.json';
 
 export default function GradesTable({ selectedStudent }) {
   const [gradesData, setGradesData] = useState([]);
@@ -55,9 +28,19 @@ export default function GradesTable({ selectedStudent }) {
       }
     };
 
-    setLevelOptions(optionLevels.classificationLevels);
+    const fetchLevelOptions = async () => {
+      try {
+        const levelsResponse = await GradesLevelsQuery();
+        const levelsData = await levelsResponse.json();
+        const levels = levelsData?.data?.acclassificationPage || [];
+        setLevelOptions(levels);
+      } catch (error) {
+        console.error('Failed to fetch level options:', error);
+      }
+    };
 
     fetchData();
+    fetchLevelOptions();
   }, [selectedStudent]);
 
   const handleEditClick = (index, columnIndex) => {
@@ -70,7 +53,6 @@ export default function GradesTable({ selectedStudent }) {
 
   const handleSelectChange = (event, index) => {
     const { value } = event.target;
-    console.log(value);
     setEditableColumns((prevColumns) => {
       const updatedColumns = [...prevColumns];
       updatedColumns[index] = value;
@@ -117,88 +99,89 @@ export default function GradesTable({ selectedStudent }) {
   if (loading) {
     return <div>Loading grades...</div>;
   }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return (
-    <div>
-      <h2>Grades</h2>
-      {gradesData.length === 0 ? (
-        <div>No grades available.</div>
-      ) : (
-        <table className="table table-striped table-hover table-sm">
-          <thead>
-            <tr>
-              <th>Subject</th>
-              <th>Semester</th>
-              <th>Level 1</th>
-              <th>Level 2</th>
-              <th>Level 3</th>
-              <th>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {gradesData.map((grade, index) => {
-              const selectedGrade = levelOptions.find(
-                (level) => level.id === editableColumns[index]
-              );
-              return (
-                <tr key={grade.id}>
-                  <td>{grade.semester?.subject?.name}</td>
-                  <td>{grade.semester?.order}</td>
-
-                  {grade.semester?.classifications
-                    ?.slice(0, 3)
-                    .map((classification, i) => {
-                      const isEditable = editableColumns[index] === i;
-                      return (
-                        <td key={classification.id}>
-                          {isEditable ? (
-                            <select
-                              className="form-select"
-                              value={selectedGrade?.id || ""}
-                              onChange={(event) =>
-                                handleSelectChange(event, index)
-                              }
-                            >
-                              {levelOptions.map((level) => (
-                                <option key={level.id} value={level.id}>
-                                  {level.name} {level.id}
-                                </option>
-                              ))}
-                            </select>
-                          ) : (
-                            <>
-                              {classification.name}
-                              <button
-                                className="btn btn-link"
-                                onClick={() => handleEditClick(index, i)}
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    }
+  
+    return (
+      <div>
+        <h2>Grades</h2>
+        {gradesData.length === 0 ? (
+          <div>No grades available.</div>
+        ) : (
+          <table className="table table-striped table-hover table-sm">
+            <thead>
+              <tr>
+                <th>Subject</th>
+                <th>Semester</th>
+                <th>Level 1</th>
+                <th>Level 2</th>
+                <th>Level 3</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gradesData.map((grade, index) => {
+                const selectedGrade = levelOptions.find(
+                  (level) => level.level.id === editableColumns[index]
+                )?.level.level;
+                return (
+                  <tr key={grade.id}>
+                    <td>{grade.semester?.subject?.name}</td>
+                    <td>{grade.semester?.order}</td>
+  
+                    {grade.semester?.classifications
+                      ?.slice(0, 3)
+                      .map((classification, i) => {
+                        const isEditable = editableColumns[index] === i;
+                        return (
+                          <td key={classification.level.id}>
+                            {isEditable ? (
+                              <select
+                                className="form-select"
+                                value={selectedGrade?.id || ""}
+                                onChange={(event) => handleSelectChange(event, index)}
                               >
-                                Edit
-                              </button>
-                            </>
-                          )}
-                        </td>
-                      );
-                    })}
-
-                  {grade.semester?.classifications &&
-                    grade.semester?.classifications.length < 3 && (
-                      Array(3 - grade.semester?.classifications.length)
-                        .fill()
-                        .map((_, i) => <td key={i}>-</td>)
-                    )}
-
-                  <td>{grade.type?.name}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
-    </div>
->>>>>>> Stashed changes
-  );
-}
+                                {levelOptions.map((level) => (
+                                  <option
+                                    key={level.level.id}
+                                    value={level.level.id}
+                                  >
+                                    {level.level.name}
+                                  </option>
+                                ))}
+                              </select>
+                            ) : (
+                              <>
+                                {classification.level.name}
+                                <button
+                                  className="btn btn-link"
+                                  onClick={() =>
+                                    handleEditClick(index, i)
+                                  }
+                                >
+                                  Edit
+                                </button>
+                              </>
+                            )}
+                          </td>
+                        );
+                      })}
+  
+                    {grade.semester?.classifications &&
+                      grade.semester?.classifications.length < 3 && (
+                        Array(3 - grade.semester?.classifications.length)
+                          .fill()
+                          .map((_, i) => <td key={i}>-</td>)
+                      )}
+  
+                    <td>{grade.type?.name}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+  }
