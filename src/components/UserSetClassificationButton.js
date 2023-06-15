b5f1507654815819ee067d357546b47dcbf6a88b
RiccieS <riccigames82@gmail.com>
2023-06-15 08:50:15 +0200

    mutation pro vice useru

--- src/components/UserSetClassificationButton.js
+++ src/components/UserSetClassificationButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { useDispatch } from 'react-redux';
 import { loadClassification } from '../slices/GradesTableSlice';
 import Button from "react-bootstrap/Button"
@@ -7,7 +7,7 @@ import { UserClassificationMutationQuery } from 'queries/UserClassificationMutat
 export const UserSetClassificationButton = ({ classification, newlevel }) => {
   const dispatch = useDispatch();
 
-  const mutateData = () => (dispatch, getState) => {
+ /* const mutateData = () => (dispatch, getState) => {
 
     console.log(classification)
 
@@ -23,8 +23,9 @@ export const UserSetClassificationButton = ({ classification, newlevel }) => {
         }
         return json
       })
-  };
-
+  };*/
+  
+/*
   const onClick = () => {
 
     dispatch(mutateData(classification));
@@ -32,6 +33,49 @@ export const UserSetClassificationButton = ({ classification, newlevel }) => {
   };
 
   return <Button onClick={() => {onClick()}}>{newlevel.name}</Button>;
+};
+
+export default UserSetClassificationButton;*/
+
+
+
+
+
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateClassification } from '../slices/GradesTableSlice';
+import Button from 'react-bootstrap/Button';
+import { UserClassificationMutationQuery } from 'queries/UserClassificationMutationQuery';
+
+export const UserSetClassificationButton = ({ classification, newlevel }) => {
+  const dispatch = useDispatch();
+  const gradesTable = useSelector((state) => state.gradesTable);
+
+  useEffect(() => {
+    // Empty dependency array, runs only once when the component mounts
+  }, []);
+
+  const mutateData = () => (dispatch, getState) => {
+    UserClassificationMutationQuery(classification.id, classification.lastchange, newlevel.id)
+      .then(response => response.json())
+      .then(json => {
+        const updatedUser = json.data?.result.classification.user;
+        if (updatedUser) {
+          const result = {result: updatedUser}
+          // Dispatch the 'loadClassification' action with the updated gradesTable
+          console.log(result)
+          dispatch(updateClassification(result));
+        }
+      });
+  };
+  
+
+  const onClick = () => {
+    dispatch(mutateData(classification));
+  };
+
+  return <Button onClick={onClick}>{newlevel.name}</Button>;
 };
 
 export default UserSetClassificationButton;
